dbcc2fffefd1e754125dcd59ec10a22c019ee5d1
frantamayo0 <ftamcay0701@g.educaand.es>
2025-02-03 12:45:39 +0100

    Update script-leer-numeros.js

--- script-leer-numeros.js
+++ script-leer-numeros.js
@@ -9,15 +9,4 @@
             numero = Number (numero); // Convertimos el texto a numero
             }
                      
-          
-            var suma = 0; 
-            var i = 1;
 
-           
-            for (i = 1; i <= numero; i++) {
-            suma = suma + i;
-            document.write ("<br>La suma de todos los numeros introducidos entre 1 y " + i + " es: " + suma);
-            }
-
-           
-            document.write ("<br><br>La suma TOTAL de todos los numeros introducidos entre 1 y " + numero + " es: " + suma);
