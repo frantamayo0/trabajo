940ac3a4a32395c4bf1f06df2fd27f08a178469e
frantamayo0 <ftamcay0701@g.educaand.es>
2025-02-12 13:05:23 +0100

    Update script-leer-numeros.js

--- script-leer-numeros.js
+++ script-leer-numeros.js
@@ -2,7 +2,7 @@
             numero = Number(numero);
             while (numero < 1 || numero > 1000)
                         {
-  numero = prompt("Número inválido. Introduce un número entre 1 y 100:");
-  numero = Number (numero);
+                          numero = prompt("Número inválido. Introduce un número entre 1 y 100:");
+                          numero = Number (numero);
                         }
             document.write ("<br>");
