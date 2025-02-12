db0265ebbf8b1371047d6ab4fb65d64008dc95b7
frantamayo0 <ftamcay0701@g.educaand.es>
2025-02-12 13:01:26 +0100

    Update script-leer-numeros.js

--- script-leer-numeros.js
+++ script-leer-numeros.js
@@ -1,6 +1,6 @@
-   var numero =  prompt("Introduce un número entre 1 y 10000:");
+   var numero =  prompt("Introduce un número entre 1 y 1000:");
             numero = Number(numero);
-            while (numero < 1 || numero > 10000)
+            while (numero < 1 || numero > 1000)
                         {
   numero = prompt("Número inválido. Introduce un número entre 1 y 100:");
   numero = Number (numero);
