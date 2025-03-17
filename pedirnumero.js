bc4c100a2694c7969c0ba0e3067a6aad73e1a744
frantamayo0 <ftamcay0701@g.educaand.es>
2025-03-17 12:43:48 +0100

    Update pedirnumero.js

--- pedirnumero.js
+++ pedirnumero.js
@@ -7,3 +7,11 @@ function pedirNumeroEnRango(min, max) {
 
   return numero;
 }
+function pedirNumeroEnRangoMejorado (texto,min,max)
+  {
+    var numero;
+    do {
+       numero = prompt(texto + min +" y " + max + ":");
+       numero = Number(numero)
+       } while (numero < min 
+      
