37d2246f92b19cecec97072cd0aaf8ca43400d48
frantamayo0 <ftamcay0701@g.educaand.es>
2025-05-05 12:33:37 +0200

    Create factorial2.js

---
+++ factorial2.js
@@ -0,0 +1,15 @@
+function Factorial (numero)
+{
+
+  var i;
+  var factorial = 1;
+
+ for (i=1; i<=numero; i++)
+                 {
+                   
+                     factorial = factorial * i;
+                     
+                 }
+
+
+return factorial;    
